405868ff52642307da9ff5998c7cd4b50d49e80f
POAL <pbraginets48@gmail.com>
2025-12-19 00:00:49 +0300

    😭😭😭😭😭😭😭😭

--- src/main/index.ts
+++ src/main/index.ts
@@ -108,8 +108,8 @@ function createWindow(): void {
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
-    // mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
-    mainWindow.loadURL("https://example.com")
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    // // mainWindow.loadURL("https://example.com")
   }
 
   mainWindow.webContents.once('did-finish-load', () => {
