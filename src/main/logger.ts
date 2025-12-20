953331e7c4bb66600e2fad9e19d43f713d0ed492
POAL <pbraginets48@gmail.com>
2025-12-20 22:46:12 +0300

    Logger refactored

--- src/main/logger.ts
+++ src/main/logger.ts
@@ -1,23 +1,25 @@
-import { appendFile, mkdir, writeFileSync } from "fs";
+// // // // import { appendFile, mkdir, writeFileSync } from "fs";
 
-let ts: string = ((date => {return date.toLocaleDateString().replace("/", ".").replace("/", ".").replace("/", ".") + '-' + date.toLocaleTimeString().replace(":", ".").replace(":", ".")})(new Date()));
+// // // // let ts: string = ((date => {return date.toLocaleDateString().replace("/", ".").replace("/", ".").replace("/", ".") + '-' + date.toLocaleTimeString().replace(":", ".").replace(":", ".")})(new Date()));
 
-let order: Array<string> = [];
-let isBusy = false;
+import log from 'electron-log/main';
+
+// // // // let order: Array<string> = [];
+// // // // let isBusy = false;
 
 export let logsRecords: Array<string> = [];
 
 let callbackUpdate = (_log: string) => {}
 
-function handleOrder() {
-    if ( order.length === 0 ) {
-        isBusy = false;
-        return;
-    }
-    const data = order.shift();
-    data ? addToRecords(data) : '';
-    appendFile("./logs/" + ts + ".log", data + "\n", _err => {handleOrder()})
-}
+// // // // function handleOrder() {
+// // // //     if ( order.length === 0 ) {
+// // // //         isBusy = false;
+// // // //         return;
+// // // //     }
+// // // //     const data = order.shift();
+// // // //     data ? addToRecords(data) : '';
+// // // //     appendFile("./logs/" + ts + ".log", data + "\n", _err => {handleOrder()})
+// // // // }
 
 function addToRecords(log: string) {
     logsRecords.push(log);
@@ -25,16 +27,19 @@ function addToRecords(log: string) {
 }
 
 export function loggerInit() {
-    mkdir("./logs", () => {});
-    ts = (date => {return date.toLocaleDateString().replace("/", ".").replace("/", ".").replace("/", ".") + '-' + date.toLocaleTimeString().replace(":", ".").replace(":", ".")})(new Date());
-    console.log(ts)
-    // writeFile("./logs/" + ts + ".log", new Date().toTimeString() + " : " + "loggerInited" + "\n", { flag: 'w+' }, _err => {console.log(_err)});
-    writeFileSync("./logs/" + ts + ".log", new Date().toLocaleTimeString() + " : " + "logger inited" + "\n", { flag: 'w+' })//, _err => {handleOrder()});
+    // // // // mkdir("./logs", () => {});
+    // // // // ts = (date => {return date.toLocaleDateString().replace("/", ".").replace("/", ".").replace("/", ".") + '-' + date.toLocaleTimeString().replace(":", ".").replace(":", ".")})(new Date());
+    // // // // console.log(ts)
+    // // // // // writeFile("./logs/" + ts + ".log", new Date().toTimeString() + " : " + "loggerInited" + "\n", { flag: 'w+' }, _err => {console.log(_err)});
+    // // // // writeFileSync("./logs/" + ts + ".log", new Date().toLocaleTimeString() + " : " + "logger inited" + "\n", { flag: 'w+' })//, _err => {handleOrder()});
+    log.initialize();
 }
 
 export function logger(data: string) {
-    if ( !isBusy ) { isBusy = true; addToRecords(new Date().toLocaleTimeString() + " : " + data); appendFile("./logs/" + ts + ".log", new Date().toLocaleTimeString() + " : " + data + "\n", _err => {handleOrder()}); }
-    else { order.push(new Date().toLocaleTimeString() + " : " + data); }
+    // // // // if ( !isBusy ) { isBusy = true; addToRecords(new Date().toLocaleTimeString() + " : " + data); appendFile("./logs/" + ts + ".log", new Date().toLocaleTimeString() + " : " + data + "\n", _err => {handleOrder()}); }
+    // // // // else { order.push(new Date().toLocaleTimeString() + " : " + data); }
+    log.info(data);
+    addToRecords(new Date().toLocaleTimeString() + " : " + data)
 }
 
 export function setCallbackUpdate(callbackUpdate_: (log: string) => void) {
